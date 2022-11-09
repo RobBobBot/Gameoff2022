0dc93a6bacfe5c5ffb2182abe520901ba59153e4
RobBobBot <r.acatrinei2005@gmail.com>
2022-11-09 22:03:03 +0200

    added direction

--- Scripts/NPC.gd
+++ Scripts/NPC.gd
@@ -3,11 +3,26 @@ extends StaticBody2D
 class_name NPC
 
 onready var screen:Control=get_node("/root/Main/Screen/UI")
+onready var player=get_node("/root/Main/Player")
 
 var nearPlayer:bool=false
 
 func interactPressed():
-	if nearPlayer:
+	var pos=position-player.position
+	if abs(pos.x)>abs(pos.y):
+		if pos.x>0:
+			pos.x=1
+		if pos.x<0:
+			pos.x=-1
+		pos.y=0
+	else:
+		if pos.y>0:
+			pos.y=1
+		if pos.y<0:
+			pos.y=-1
+		pos.x=0
+	
+	if nearPlayer && pos==player.direction:
 		screen.toCutscene()
 
 func _ready():
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,12 +2,14 @@ extends KinematicBody2D
 
 var moving:bool = false
 var last=0
-var vel
+var velocity
 onready var map: TileMap = get_node("/root/Main/MainMap")
 onready var tween:Tween=get_node("/root/Main/MovementTween")
+onready var animation:AnimatedSprite=get_node("Animation")
+var direction:Vector2
 
 func move(val):
-	move_and_collide(vel*(val-last))
+	move_and_collide(velocity*(val-last))
 	last=val
 
 func _physics_process(delta):
@@ -15,7 +17,8 @@ func _physics_process(delta):
 		return
 		
 	if Stats.playerVelocity!=Vector2()&&!moving:
-		vel=Stats.playerVelocity
+		velocity=Stats.playerVelocity
+		get_direction(velocity)
 		moving=true
 		tween.interpolate_method(self,"move",0,1,0.1)
 		tween.start()
@@ -23,3 +26,26 @@ func _physics_process(delta):
 func _on_MovementTween_tween_all_completed():
 	moving=false
 	last=0
+
+func get_direction(velocity):
+	direction=velocity.normalized()
+
+func _process(delta):
+	if moving:
+		if direction.x==1:
+			animation.animation="MoveRight"
+		if direction.x==-1:
+			animation.animation="MoveLeft"
+		if direction.y==1:
+			animation.animation="MoveDown"
+		if direction.y==-1:
+			animation.animation="MoveUp"
+	else:
+		if direction.x==1:
+			animation.animation="IdleRight"
+		if direction.x==-1:
+			animation.animation="IdleLeft"
+		if direction.y==1:
+			animation.animation="IdleDown"
+		if direction.y==-1:
+			animation.animation="IdleUp"
